d1b979d6c536333aa8200d2a267f5adbb40aa5e2
ArcadeFortune <alessionights@gmail.com>
2023-10-04 07:39:26 +0200

    dev: adding SSE api

--- src/server.js
+++ src/server.js
@@ -55,6 +55,66 @@ app.get('/xml', (req, res) => {
   });
 });
 
+app.get("/additiveIncome", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  fs.readdir(xmlDirectory, (err, files) => {
+    if (err) {
+      res.status(500).send("Serverfehler beim Lesen des Verzeichnisses");
+      return;
+    }
+
+    const xmlFiles = files.filter((file) => file.endsWith(".xml"));
+
+    let combinedData = [];
+
+    // Wir nutzen Promises, um alle Dateien asynchron zu lesen und zu verarbeiten
+    const promises = xmlFiles.map((file) => {
+      return new Promise((resolve, reject) => {
+        const filePath = path.join(xmlDirectory, file);
+        fs.readFile(filePath, "utf8", (err, rawXmlContent) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          // heart of the code
+          const XML = parseRawXml(rawXmlContent);
+          const dataSet = convertToData(XML);
+          combinedData.push(...dataSet);
+          console.log("loading...");
+          resolve();
+        });
+      });
+    });
+
+    // Nachdem alle Dateien verarbeitet wurden
+    Promise.all(promises)
+      .then(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+          if (index < combinedData.length) {
+            const dataToSend = combinedData.slice(index, index + 2); // sendet jeweils 2 Datenobjekte
+            dataToSend.forEach((data) => {
+              res.write(`data: ${JSON.stringify(data)}\n\n`); // sendet Daten
+            });
+            index += 2;
+          } else {
+            console.log("finished");
+            clearInterval(interval);
+            res.end();
+          }
+        }, 2000); // Sendet alle zwei Sekunden Daten
+      })
+      .catch((error) =>
+        res
+          .status(500)
+          .send("Fehler bei der Verarbeitung der XML-Dateien", error)
+      );
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server läuft unter http://localhost:${port}`);
 });
