7d5aa17a279b7f4bb3318dd22c60199b7afe46db
ArcadeFortune <alessionights@gmail.com>
2023-10-04 14:06:09 +0200

    fix: each object has diffrent timestamp

--- src/convertToESLData.js
+++ src/convertToESLData.js
@@ -1,17 +1,18 @@
 const convertToESLData = (XML) => {
   const elementSell = XML.querySelector('ValueRow[obis="1-1:1.8.1"]')
   const elementBuy = XML.querySelector('ValueRow[obis="1-1:2.8.1"]')
+  const date = XML.querySelector('TimePeriod');
+  
   if (!elementSell || !elementBuy) {
-      return null;
+    return null;
   }
   const valueSell = elementSell.getAttribute('value');
   const valueBuy = elementBuy.getAttribute('value');
-  // Suche nach dem ValueRow-Element mit obis="1-1:1.8.1"
-  // const valueRowElement = document.querySelector('ValueRow[obis="1-1:1.8.1"]');
+  const endDate = date.getAttribute('end');
 
   // Extrahiere den Wert des 'value'-Attributs
   return {
-      timestamp: '2019-03-13T23:00:00.000',
+      timestamp: endDate,
       valueSell: valueSell,
       valueBuy: valueBuy
     }
