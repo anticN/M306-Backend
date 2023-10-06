888c74e6bd53a89fec3f7bdda283cdf91ba18780
anticN <anticn@bzz.ch>
2023-10-06 09:12:39 +0200

    removed not needed code from js backend

--- src/parseRawXml.js
+++
@@ -1,7 +0,0 @@
-const { JSDOM } = require("jsdom");
-
-const parseRawXml = (xmlContent) => {
-  return new JSDOM(xmlContent, { contentType: 'text/xml' }).window.document; // parse XML
-}
-
-module.exports = parseRawXml;
